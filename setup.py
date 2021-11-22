a89d0a18c6b8125f28513480ad78fc81dcd9c7e3
luozhouyang <zhouyang.luo@gmail.com>
2021-11-22 21:25:48 +0800

    Bump version 0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="naivenlp",
-    version="0.0.9",
+    version="0.1.0",
     description="Transformers based toolkit for modeling NLP tasks.",
     long_description=long_description,
     long_description_content_type="text/markdown",
